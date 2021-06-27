91a486632150382966c786b3a5a753abfe25b994
Joao Luiz <joao_luiz@id.uff.br>
2021-06-27 20:41:35 -0300

    find textbox using xPath

--- write_on_chat_troll.js
+++ write_on_chat_troll.js
@@ -1,9 +1,13 @@
-document.getElementById('chat-list-content').hidden=true$
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+document.getElementById('chat-list-content').hidden=true
 
 var quem = window.prompt("Digite o nome do participante que deseja ligar a câmera");
 window.alert("Ligando a câmera de " + quem);
 
-var demo_input = document.getElementById('txtMensagem'); //Local para inserir a mensagem
+var demo_input = getElementByXpath('//*[@id="wc-container-right"]/div/div[4]/textarea') //Local para inserir a mensagem
 
 var type_this = (quem + " eu estou no cio e queria ligar sua cam pq eu tenho resquicios de psicopatia e nao respeito a privacidade dos outros." ); //texto vergonhoso
 
@@ -14,6 +18,9 @@ window.next_letter = function() {
         demo_input.value = type_this.substr(0, index++);
         setTimeout("next_letter()", 10); //TIMER DA MENSAGEM EM MS
     }
+    //demo_input.sendKeys(ENTER);
 }
 
 next_letter();
+
+
